0a1dd361403e2d5d0bf79ca5235126cbb5768905
Andriy NESTOR <anestor@e2r5p10.unit.ua>
2017-12-30 20:22:27 +0200

    aded top, and changed gui

--- sub.js
+++ sub.js
@@ -11,30 +11,76 @@ function addSomething1(product, textureLoader, x, y, z, sizeX, sizeY, sizeZ) {
 
 function addProduct(product, scene)
 {
-	var hProfil = product.h - product.hTop - product.hBot;
+	var half = product.profileThick / 2;
+	var offset = 0.005;
+	var hProfil = product.h - product.topThick - product.botThick;
 	product.profileG = new THREE.BoxGeometry(product.profileThick, hProfil, product.profileThick);
+	addProfile(0, -product.l / 2 + half + offset, hProfil / 2 + product.botThick, -product.w / 2 + half + offset, product, scene);	
+	addProfile(1, product.l / 2 - half - offset, hProfil / 2 + product.botThick, -product.w / 2 + half + offset, product, scene);
+	addProfile(2, -product.l / 2 + half + offset, hProfil / 2 + product.botThick, product.w / 2 - half - offset, product, scene);
+	addProfile(3, product.l / 2 - half - offset, hProfil / 2 + product.botThick, product.w / 2 - half - offset, product, scene);	
 
-	addProfile(0, -product.l / 2, hProfil / 2 + product.hBot, -product.w / 2, product, scene);	
-	addProfile(1, product.l / 2, hProfil / 2 + product.hBot, -product.w / 2, product, scene);
-	addProfile(2, -product.l / 2, hProfil / 2 + product.hBot, product.w / 2, product, scene);
-	addProfile(3, product.l / 2, hProfil / 2 + product.hBot, product.w / 2, product, scene);	
-
-	var wProfil = product.w - product.profileThick;
+	var wProfil = product.w - product.profileThick * 2 - offset * 2;
 	product.profileG = new THREE.BoxGeometry(product.profileThick, product.profileThick, wProfil);
+	addProfile(4, -product.l / 2 + half + offset, product.botThick + half, 0, product, scene);
+	addProfile(5, -product.l / 2 + half + offset, hProfil + 0.010, 0, product, scene);
+	addProfile(6, product.l / 2 - half - offset, product.botThick + half, 0, product, scene);
+	addProfile(7, product.l / 2 - half - offset, hProfil + 0.010, 0, product, scene);
 
-	addProfile(4, -product.l / 2, product.hBot + product.profileThick / 2, 0, product, scene);
-	addProfile(5, -product.l / 2, hProfil + 0.010, 0, product, scene);
-	addProfile(6, product.l / 2, product.hBot + product.profileThick / 2, 0, product, scene);
-	addProfile(7, product.l / 2, hProfil + 0.010, 0, product, scene);
-
-	var	lProfil = product.l - product.profileThick;
+	var	lProfil = product.l - product.profileThick * 2 - offset * 2;
 	product.profileG = new THREE.BoxGeometry(lProfil, product.profileThick, product.profileThick);
+	addProfile(8, 0, hProfil + 0.01, product.w / 2 - half - offset, product, scene);
+	addProfile(9, 0, hProfil + 0.01, -product.w / 2 + half + offset, product, scene);
 
-	addProfile(8, 0, hProfil + 0.01, product.w / 2, product, scene);
-	addProfile(9, 0, hProfil + 0.01, -product.w / 2, product, scene);
+	product.topG = new THREE.BoxGeometry(product.l, product.topThick, product.w);
+//	var modifier = new THREE.BufferSubdivisionModifier(10);
+//	modifier.modify(product.topG);
+	product.topW = new THREE.Mesh(product.topG, product.woodMat[product.woodColor]);
+	product.topW.position.set(0, product.h - product.topThick / 2, 0);
+	product.topW.castShadow = true;
+	scene.add(product.topW);
 
+
+	product.topSquare = product.l * product.w;
+//	product.segmentQuantity = params.vDivide;
 	product.profileLen = hProfil * 4 + wProfil * 4 + lProfil * 2;
 
+}
+
+function addWoodMaterial(index, product, mat1, mat2, mat3)
+{
+	product.woodMat[index] = new THREE.MeshStandardMaterial( {
+		roughness: 0.7,
+		color: 0xffffff,
+		bumpScale: 0.002,
+		metalness: 0.5
+	} );
+	
+	var texturesLoader = new THREE.TextureLoader();
+	texturesLoader.load( mat1 , function( map ) {
+		map.wrapS = THREE.RepeatWrapping;
+		map.wrapT = THREE.RepeatWrapping;
+		map.anisotropy = 4;
+		map.repeat.set( 1, 2.4 );
+		product.woodMat[index].map = map;
+		product.woodMat[index].needsUpdate = true;
+	} );
+	texturesLoader.load( mat2, function( map ) {
+		map.wrapS = THREE.RepeatWrapping;
+		map.wrapT = THREE.RepeatWrapping;
+		map.anisotropy = 4;
+		map.repeat.set( 1, 2.4 );
+		product.woodMat[index].bumpMap = map;
+		product.woodMat[index].needsUpdate = true;
+	} );
+	texturesLoader.load( mat3, function( map ) {
+		map.wrapS = THREE.RepeatWrapping;
+		map.wrapT = THREE.RepeatWrapping;
+		map.anisotropy = 4;
+		map.repeat.set( 1, 2.4 );
+		product.woodMat[index].roghnessMap = map;
+		product.woodMat[index].needsUpdate = true;
+	} );
 }
 
 function addProfileMaterial(index, product, mat1, mat2, mat3)
@@ -50,11 +96,10 @@ function addProfileMaterial(index, product, mat1, mat2, mat3)
 		map.wrapS = THREE.RepeatWrapping;
 		map.wrapT = THREE.RepeatWrapping;
 		map.anisotropy = 4;
-		map.repeat.set( 50, 120 );
+		map.repeat.set( 10, 24 );
 		product.profileMat[index].map = map;
 		product.profileMat[index].needsUpdate = true;
 	} );
-	
 	texturesLoader.load( mat2, function( map ) {
 		map.wrapS = THREE.RepeatWrapping;
 		map.wrapT = THREE.RepeatWrapping;
@@ -86,4 +131,49 @@ function delProduct(product, scene)
 	var n = 0;
 	while (product.profile[n])
 		scene.remove(product.profile[n++]);
+	scene.remove(product.topW);
+}
+
+function guiCreate()
+{
+	params.quantity = 0; // ned to be set to needed value
+	gui = new dat.GUI();
+	gui.add( params, 'hemiIrradiance', Object.keys( hemiLuminousIrradiances ) );
+	gui.add( params, 'bulbPower', Object.keys( bulbLuminousPowers ) );
+	gui.add( params, 'exposure', 0, 1 );
+	gui.add( params, 'shadows' );
+	gui.add( params, 'profileColor', Object.keys( profileColorChoose ) );
+	gui.add( params, 'woodColor', Object.keys( woodColorChoose ) );
+	gui.add( params, 'h', 0.2, 2 );
+	gui.add( params, 'w', 0.2, 1 );
+	gui.add( params, 'l', 0.2, 2 );
+	gui.add( params, 'options', Object.keys( segmentOptions) );
+/*
+	if (segmentOptions[params.options] == 0)
+	{
+		                          //// handle max 
+	}
+	else if (segmentOptions[params.options] == 1)
+	{
+	}
+	else if (segmentOptions[params.options] == 2)
+	{
+		                              ///// handle max
+	}
+*/	
+	gui.add( params, 'quantity', 0, 10 ).step(1); //handle max
+//	for (var i = 0; i < params.vDivide; i++)
+//	{
+//		if (params.vDivide > 0)
+//			vFolders[i] = gui.add( params, 'test' );
+//	}
+//	var ggg = gui.add (params, 'test' );
+//	gui.remove(ggg);
+//	gui.add( params, 'profileLen').listen();
+//	for (var n = 0; n != product.segmentQuantity; n++)
+//	{
+//	if (product.segmentQuantity > 0)
+//		gui.add( params, 'test' );
+//	}
+	gui.open();
 }
